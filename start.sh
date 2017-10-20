69252f906493dbf6c4ba4ed68b5654c291f8fa59
Vitor Arins <vitorarins@gmail.com>
2017-10-20 15:41:11 -0200

    Remove template databases from backup script; Add confirmation for overwrite on azure-cli commands; Remove unnecessary line.

--- start.sh
+++ start.sh
@@ -28,7 +28,7 @@ if [ "$1" == "backup" ]; then
     if [ -n "$2" ]; then
         databases=$2
     else
-        databases=`psql --username=$POSTGRES_USER --host=$POSTGRES_HOST --port=$POSTGRES_PORT -l | grep "UTF8" | awk '{print $1}'`
+        databases=`psql --username=$POSTGRES_USER --host=$POSTGRES_HOST --port=$POSTGRES_PORT -l | grep "UTF8" | grep -Ev "(template[0-9]*)" | awk '{print $1}'`
     fi
 
     for db in $databases; do
@@ -37,7 +37,7 @@ if [ "$1" == "backup" ]; then
         pg_dump --host=$POSTGRES_HOST --port=$POSTGRES_PORT --username=$POSTGRES_USER $db | gzip > "/tmp/$db.gz"
 
         if [ $? == 0 ]; then
-            /usr/local/bin/azure storage blob upload /tmp/$db.gz $AZURE_STORAGE_CONTAINER -c "DefaultEndpointsProtocol=https;BlobEndpoint=https://$AZURE_STORAGE_ACCOUNT.blob.core.windows.net/;AccountName=$AZURE_STORAGE_ACCOUNT;AccountKey=$AZURE_STORAGE_ACCESS_KEY"
+            yes | /usr/local/bin/azure storage blob upload /tmp/$db.gz $AZURE_STORAGE_CONTAINER -c "DefaultEndpointsProtocol=https;BlobEndpoint=https://$AZURE_STORAGE_ACCOUNT.blob.core.windows.net/;AccountName=$AZURE_STORAGE_ACCOUNT;AccountKey=$AZURE_STORAGE_ACCESS_KEY"
 
             if [ $? == 0 ]; then
                 rm /tmp/$db.gz
@@ -61,7 +61,7 @@ elif [ "$1" == "restore" ]; then
         echo "restoring $archive"
         echo "...transferring"
 
-        /usr/local/bin/azure storage blob download  -a $AZURE_STORAGE_ACCOUNT -k "$AZURE_STORAGE_ACCESS_KEY" $AZURE_STORAGE_CONTAINER $archive $tmp
+        yes | /usr/local/bin/azure storage blob download  -a $AZURE_STORAGE_ACCOUNT -k "$AZURE_STORAGE_ACCESS_KEY" $AZURE_STORAGE_CONTAINER $archive $tmp
 
         if [ $? == 0 ]; then
             echo "...restoring"
@@ -69,7 +69,6 @@ elif [ "$1" == "restore" ]; then
 
             psql --host=$POSTGRES_HOST --port=$POSTGRES_PORT --username=$POSTGRES_USER -d $db -c "drop schema public cascade; create schema public;"
 
-            psql --host=$POSTGRES_HOST --port=$POSTGRES_PORT --username=$POSTGRES_USER -d $db
             gunzip -c $tmp | psql --host=$POSTGRES_HOST --port=$POSTGRES_PORT --username=$POSTGRES_USER -d $db
         else
             rm $tmp
